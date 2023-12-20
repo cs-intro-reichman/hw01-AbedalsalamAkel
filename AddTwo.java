c8358a24c3d7232062793394391677d66e2b5866
AbedalsalamAkel <154346157+AbedalsalamAkel@users.noreply.github.com>
2023-12-20 22:21:42 +0200

    Update AddTwo.java
    tried alot please work

--- AddTwo.java
+++ AddTwo.java
@@ -1,11 +1,12 @@
 /*
  * Adds two given integers and prints the result in a fancy way.
  */
-class AddTwo{
- public static void main(String[] args){
-    int a = Integer.parseInt(args[0]);
-    int b = Integer.parseInt(args[1]);
-    int c = a + b ;
-        System.out.println( a + " " + " + " + " " + b + " " +  " = " + " " +   c);
- }
+class AddTwo {
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = a + b;
+        System.out.println(a + " + " + b + " = " + c);
+    }
 }
+
